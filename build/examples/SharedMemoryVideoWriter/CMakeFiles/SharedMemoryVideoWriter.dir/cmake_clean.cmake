2cec5b822fc3dc6de8d8e5ca0025cc34130b14e9
lss <shaoshuai_luo@126.com>
2022-04-28 19:13:34 +0800

    old version

---
+++ build/examples/SharedMemoryVideoWriter/CMakeFiles/SharedMemoryVideoWriter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SharedMemoryVideoWriter.dir/main.cpp.o"
+  "SharedMemoryVideoWriter.pdb"
+  "SharedMemoryVideoWriter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SharedMemoryVideoWriter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/pangolin.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lss/Downloads/Pangolin_0.5/build/src/fonts.c" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/fonts.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "src/include"
+  "/usr/include/eigen3"
+  "/usr/include/python3.5m"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/ni"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lss/Downloads/Pangolin_0.5/src/console/ConsoleView.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/console/ConsoleView.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/device/display_x11.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/device/display_x11.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/display.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/display.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/opengl_render_state.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/opengl_render_state.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/view.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/view.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/viewport.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/viewport.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/display/widgets/widgets.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/display/widgets/widgets.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/glchar.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/glchar.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/gldraw.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/gldraw.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/glfont.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/glfont.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/glpangoglu.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/glpangoglu.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/gltext.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/gltext.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/gl/gltexturecache.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/gl/gltexturecache.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/handler/handler.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/handler/handler.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/handler/handler_glbuffer.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/handler/handler_glbuffer.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/handler/handler_image.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/handler/handler_image.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/image/image_common.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/image/image_common.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/image/image_io.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/image/image_io.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/log/packetstream.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/log/packetstream.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/plot/datalog.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/plot/datalog.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/plot/plotter.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/plot/plotter.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/python/PyInterpreter.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/python/PyInterpreter.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/python/PyModulePangolin.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/python/PyModulePangolin.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/file_extension.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/file_extension.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/file_utils.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/file_utils.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/posix/condition_variable.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/posix/condition_variable.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/posix/semaphore.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/posix/semaphore.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/posix/shared_memory_buffer.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/posix/shared_memory_buffer.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/sigstate.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/sigstate.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/threadedfilebuf.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/threadedfilebuf.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/timer.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/timer.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/utils/uri.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/utils/uri.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/var/input_record_repeat.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/var/input_record_repeat.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/var/vars.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/var/vars.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/debayer.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/debayer.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/ffmpeg.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/ffmpeg.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/images.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/images.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/join.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/join.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/mirror.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/mirror.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/openni.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/openni.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/pango_video.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/pango_video.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/pango_video_output.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/pango_video_output.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/pvn_video.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/pvn_video.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/shared_memory.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/shared_memory.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/shift.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/shift.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/test.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/test.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/thread.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/thread.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/unpack.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/unpack.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/v4l.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/v4l.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/drivers/video_splitter.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/drivers/video_splitter.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/video.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/video.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/video_output.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/video_output.cpp.o"
+  "/home/lss/Downloads/Pangolin_0.5/src/video/video_record_repeat.cpp" "/home/lss/Downloads/Pangolin_0.5/build/src/CMakeFiles/pangolin.dir/video/video_record_repeat.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "src/include"
+  "/usr/include/eigen3"
+  "/usr/include/python3.5m"
+  "/usr/include/x86_64-linux-gnu"
+  "/usr/include/ni"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
